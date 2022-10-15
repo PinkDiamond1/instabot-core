df457ce2dbb39208b9030c65fc8430da51e30800
hebertcisco <hebertcisco@outlook.com>
2022-10-15 20:26:31 -0300

    refactor: Redone mjs example, adapting the new settings

--- examples/mjs/index.mjs
+++ examples/mjs/index.mjs
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-import * as instabot from 'instabot-core';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const { commentService } = instabot;
-
-const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
-
-async function* commentForever() {
-  yield commentService({
-    link: IG_LINK,
-    author: PHRASE_AUTHOR,
-    loginInstagram: IG_USERNAME,
-    passwordInstagram: IG_PASSWORD,
-  });
-}
+import 'dotenv/config';
+import { CommentService } from '../../lib/index.js';
 
 (async () => {
-  await commentForever().next();
+  const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
+
+  const commentService = new CommentService({
+    loginInstagram: IG_USERNAME,
+    passwordInstagram: IG_PASSWORD,
+    puppeteer_options: {
+      headless: true,
+    },
+  });
+  function* commentForever() {
+    yield commentService.execute({
+      link: IG_LINK,
+      author: PHRASE_AUTHOR,
+    });
+  }
+
+  commentForever().next();
 })();
