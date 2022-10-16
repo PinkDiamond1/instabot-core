2cb303812cc8d5d1c340f757207ee3d4a474932b
hebertcisco <hebertcisco@outlook.com>
2022-10-15 23:37:47 -0300

    feat:

--- examples/cjs/index.cjs
+++ examples/cjs/index.cjs
@@ -1,9 +1,10 @@
-#!/usr/bin / env node
-require("dotenv/config");
+#!/usr/bin/env node
+require('dotenv/config');
 const { CommentService } = require('../../lib/index.js');
-const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
 
 (async () => {
+  const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
+
   const commentService = new CommentService({
     loginInstagram: IG_USERNAME,
     passwordInstagram: IG_PASSWORD,
