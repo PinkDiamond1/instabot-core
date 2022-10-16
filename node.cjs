6c79f06f6b9bbc3561f161ebf1b64ca1f9097a48
Hebert  F. Barros <tecnobertinfo@gmail.com>
2022-10-16 17:17:40 -0300

    Update node.cjs

--- node.cjs
+++ node.cjs
@@ -16,20 +16,3 @@ const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
         author: PHRASE_AUTHOR,
     });
 })();
-
-
-const commentService = new CommentService({
-    loginInstagram: IG_USERNAME,
-    passwordInstagram: IG_PASSWORD,
-    puppeteer_options: {
-        headless: true,
-    },
-});
-function* commentForever() {
-    yield commentService.execute({
-        link: IG_LINK,
-        author: PHRASE_AUTHOR,
-    });
-}
-
-commentForever().next();
